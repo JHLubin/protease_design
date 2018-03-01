e9b090f110f0d400a599ec642141decf53dfca82
jhlubin <jlubin3@jhu.edu>
2018-03-01 16:49:44 -0500

    Added a function to condense_fasc that matches columns for the mutation section so I don't need to do that manually.

--- condense_fasc.py
+++ condense_fasc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 """
 Combines FASC files from PyRosetta job distributor and converts them into a 
-tabular form
+tabular form. 
 """
 import argparse
 from glob import glob
@@ -13,6 +13,23 @@ def parse_args():
 		help="Read fasc files from what directory?")
 	args = parser.parse_args()
 	return args
+
+
+def cleanup_mutations_section(report_lines, start_point):
+	""" 
+	Some sets are missing mutable residues present in others. This function 
+	maintiains column alignment across all sets by inserting what will appear
+	as blank cells where a mutable residue is missing.
+	"""
+	max_len = max([len(line) for line in report_lines])
+	res_columns = range(start_point, max_len, 4)
+
+	for c in res_columns:
+		first_des_res = min([int(line[c]) for line in report_lines])
+		for line in report_lines:
+			if int(line[c]) != first_des_res:
+				for i in range(4):
+					line.insert(c, '=""')
 
 
 def main():
@@ -29,40 +46,47 @@ def main():
 		if 'combined_reports' in i:
 			fasc_files.pop(n)
 
+	# Collecting fasc lines
+	headline = []
+	report_lines = []
+	mut_section_ind = 0
+	for f in fasc_files:
+		# Reading in individual FASC file
+		with open(f, 'r') as read:
+			f_lines = read.readlines()
+			f_lines.pop(0) # First line is not useful
+			lines_data = []
+			for i in f_lines:
+				start_mutations = i.find('mutations')
+				scores_section = i[:start_mutations].split()
+				prot_mutations_section = i[start_mutations:].split()
+				line_data = scores_section[1::2] + prot_mutations_section
+				if i == f_lines[0] and f == fasc_files[0]:
+					headline = scores_section[::2]
+					mut_section_ind = len(scores_section) / 2 + 1
+				lines_data.append(line_data)
+			lines_data.sort(key=lambda x: x[1]) # Sorting by total score
+			report_lines += lines_data
+
 	# Making combined report
 	report_name = folder.rstrip('/') + '_combined_reports.fasc'
 	report_name = join(folder, report_name)
+
 	with open(report_name, 'w') as r:
-		for f in fasc_files:
-			# Reading in individual FASC file
-			with open(f, 'r') as read:
-				f_lines = read.readlines()
-				f_lines.pop(0) # First line is not useful
-				lines_data = []
-				for i in f_lines:
-					start_mutations = i.find('mutations')
-					scores_section = i[:start_mutations].split()
-					prot_mutations_section = i[start_mutations:].split()
-					line_data = scores_section[1::2] + prot_mutations_section
-					if i == f_lines[0]:
-						headline = scores_section[::2]
-					lines_data.append(line_data)
-				lines_data.sort(key=lambda x: x[1]) # Sorting by total score
+		# Making template and header
+		head_length = len(headline)
+		template = '{:50s}' + '{:25s}' * (head_length - 1)
+		r.write(template.format(*headline) + '\n')
 
-			if f == fasc_files[0]:
-				# Making template and header
-				head_length = len(headline)
-				template = '{:50s}' + '{:25s}' * (head_length - 1)
-				r.write(template.format(*headline) + '\n')
+		# Adding in lines
+		cleanup_mutations_section(report_lines, mut_section_ind)
+		for line in report_lines:
+			line[0] = basename(line[0])
+			line_out = template.format(*line[:head_length])
+			line_out += '   '.join(line[head_length + 1:])
+			r.write(line_out + '\n')
 
-			# Adding in lines
-			for line in lines_data:
-				line[0] = basename(line[0])
-				line_out = template.format(*line[:head_length])
-				line_out += '   '.join(line[head_length + 1:])
-				r.write(line_out + '\n')
-
-	print report_name
+		print report_name
 
 if __name__ == '__main__':
 	main()
